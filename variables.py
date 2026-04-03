1bb5f47c3745978814bf44f755e92274e07525b4
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-03 22:33:10 +0530

    k8-rbac

---
+++ variables.py
@@ -0,0 +1,14 @@
+num_servers = 10
+print(num_servers)
+CONST_PI = 3.14159
+print(CONST_PI)
+
+num_servers = 15
+print(num_servers)
+print(type(num_servers))
+      
+server = "Ansible Server"
+print(type(server))
+
+is_prod = False
+print(is_prod)
